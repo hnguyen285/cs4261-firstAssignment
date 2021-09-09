7672a7beed88d4260b0600cedf45d79f50a5934e
Hao Nguyen <dinhhao285@gmail.com>
2021-09-08 22:47:37 -0400

    Initial Commit

---
+++ gandom/gandomApp.swift
@@ -0,0 +1,17 @@
+//
+//  gandomApp.swift
+//  gandom
+//
+//  Created by Hao Nguyen on 9/8/21.
+//
+
+import SwiftUI
+
+@main
+struct gandomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

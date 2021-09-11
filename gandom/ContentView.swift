f2512decf5d8017eaefd0fc0a6d71065e64a079b
Hao Nguyen <dinhhao285@gmail.com>
2021-09-10 23:21:52 -0400

    adds app icon and screen where users can shake dice

--- gandom/ContentView.swift
+++ gandom/ContentView.swift
@@ -2,15 +2,33 @@
 //  ContentView.swift
 //  gandom
 //
-//  Created by Hao Nguyen on 9/8/21.
+//  Created by Hao Nguyen, Tuan Ho on 9/8/21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var diceFaceRand = Int.random(in: 2...7)
+        
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            Image(String(diceFaceRand))
+                .aspectRatio(contentMode: .fit)
+            Spacer()
+            Button(action: {
+                diceFaceRand = Int.random(in: 2...7)
+            }, label: {
+                Text("SHAKE")
+                    .font(.title)
+                    .fontWeight(.black)
+                    .foregroundColor(Color.black).padding(5.0)
+            })
+            .padding(3.0)
+            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/4/*@END_MENU_TOKEN@*/)
+            Spacer()
+        }
     }
 }
 
